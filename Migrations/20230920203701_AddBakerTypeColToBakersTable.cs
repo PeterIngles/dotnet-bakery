ec742c5fc163b00d8ce3395ae9d26ca3b91849db
Peter Ingles <peter.ingles@gmail.com>
2023-09-20 15:47:32 -0500

    Add Baker model, migrations to creat Baker table

---
+++ Migrations/20230920203701_AddBakerTypeColToBakersTable.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DotnetBakery.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddBakerTypeColToBakersTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "bakerType",
+                table: "Bakers",
+                type: "text",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "bakerType",
+                table: "Bakers");
+        }
+    }
+}
--- Models/ApplicationContext.cs
+++ Models/ApplicationContext.cs
@@ -4,5 +4,11 @@ namespace DotnetBakery.Models
     public class ApplicationContext : DbContext
     {
         public ApplicationContext(DbContextOptions<ApplicationContext> options) : base(options) {}
+
+        //Here is where we tell EF which of our models we'd like to have db tables for
+        public DbSet<Baker> Bakers {get; set;}
+        //             👆      👆
+        //    name of class   name of the db table
     }
 }
+
